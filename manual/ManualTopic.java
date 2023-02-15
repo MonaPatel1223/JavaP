4952acaab0a4d7cd5fa6c7fb87ecb4ed64607a68
MonaPatel1223 <minalthakor1223@gmail.com>
2023-02-15 02:07:46 +0000

    practice

---
+++ manual/ManualTopic.java
@@ -0,0 +1,15 @@
+package manual;
+
+public class ManualTopic
+    // change the classname Manual to ManualTopic
+{
+    public static void main(String[] args)
+    {
+        System.out.println("What is class");
+        System.out.println("What is object");
+        System.out.println("What is variable");
+        System.out.println("What is method");
+        System.out.println("What is subject");
+
+    }
+}
---
+++ testingjava/Topic1.java
@@ -0,0 +1,19 @@
+package testingjava;
+
+/* change the package name  to java */
+public class Topic1
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Software Testing");
+        System.out.println(" Testing Benefits");
+        System.out.println("Testing challenges");
+        System.out.println("Testing types");
+        System.out.println("Testing Levels");
+        System.out.println("Testing Principle");
+        System.out.println("BDD Testing");
+        System.out.println("TDD Testing");
+        System.out.println("Models");
+        System.out.println("Testing Lifecycle");
+    }
+}
